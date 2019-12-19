26b174af319d599fde4058d1d54ce6e0e90d6013
Pongnut Jittipanyakul <p.jittipanyakul@gmail.com>
2019-12-20 02:40:26 +0700

    Add links to social media

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -4,9 +4,33 @@ import "../App.css";
 function Introduction() {
   return (
     <div>
-      <img src={require("../images/profile.jpg")} alt="Profile" className="App-logo" />
-      <h1>Pongnut Jittipanyakul</h1>
-      <p>Coming soon</p>
+      <div>
+        <img src={require("../images/profile.jpg")} alt="Profile" className="App-logo" />
+        <h1>Pongnut Jittipanyakul</h1>
+        <p>Coming soon</p>
+      </div>
+      <div className="App-social-box">
+        <div>
+          <a href="https://facebook.com/ppprues/" target="_blank" rel="noopener noreferrer">
+            <img src={require("../assets/facebook512.svg")} alt="Facebook" className="App-social-logo" />
+          </a>
+        </div>
+        <div>
+          <a href="https://github.com/ppprues/" target="_blank" rel="noopener noreferrer">
+            <img src={require("../assets/github512.svg")} alt="GitHub" className="App-social-logo" />
+          </a>
+        </div>
+        <div>
+          <a href="https://instagram.com/ppprues/" target="_blank" rel="noopener noreferrer">
+            <img src={require("../assets/instagram512.svg")} alt="Instagram" className="App-social-logo" />
+          </a>
+        </div>
+        <div>
+          <a href="https://www.linkedin.com/in/ppprues/" target="_blank" rel="noopener noreferrer">
+            <img src={require("../assets/linkedin512.svg")} alt="LinkedIn" className="App-social-logo" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
