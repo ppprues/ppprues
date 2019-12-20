efcd1f2d6e26ea24b7859e87ab5a8c4e86acbec4
Pongnut Jittipanyakul <pongnut17@hotmail.com>
2019-12-20 15:50:58 +0700

    Add transition

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,36 +1,45 @@
 import React from "react";
+import { CSSTransitionGroup } from "react-transition-group";
 import "../App.css";
 
 function Introduction() {
   return (
     <div>
       <div>
-        <img src={require("../images/profile.jpg")} alt="Profile" className="App-logo" />
-        <h1>Pongnut Jittipanyakul</h1>
-        <p>Coming soon</p>
+        <CSSTransitionGroup transitionName="fade-1" transitionAppear={true} transitionEnter={false} transitionLeave={false}>
+          <img src={require("../images/profile.jpg")} alt="Profile" className="App-logo" />
+        </CSSTransitionGroup>
+        <CSSTransitionGroup transitionName="fade-2" transitionAppear={true} transitionEnter={false} transitionLeave={false}>
+          <h1>Pongnut Jittipanyakul</h1>
+        </CSSTransitionGroup>
+        <CSSTransitionGroup transitionName="fade-3" transitionAppear={true} transitionEnter={false} transitionLeave={false}>
+          <p>Coming soon</p>
+        </CSSTransitionGroup>
       </div>
-      <div className="App-social-box">
-        <div>
-          <a href="https://facebook.com/ppprues/" target="_blank" rel="noopener noreferrer">
-            <img src={require("../assets/facebook512.svg")} alt="Facebook" className="App-social-logo" />
-          </a>
+      <CSSTransitionGroup transitionName="fade-4" transitionAppear={true} transitionEnter={false} transitionLeave={false}>
+        <div className="App-social-box">
+          <div>
+            <a href="https://facebook.com/ppprues/" target="_blank" rel="noopener noreferrer">
+              <img src={require("../assets/facebook512.svg")} alt="Facebook" className="App-social-logo" />
+            </a>
+          </div>
+          <div>
+            <a href="https://github.com/ppprues/" target="_blank" rel="noopener noreferrer">
+              <img src={require("../assets/github512.svg")} alt="GitHub" className="App-social-logo" />
+            </a>
+          </div>
+          <div>
+            <a href="https://instagram.com/ppprues/" target="_blank" rel="noopener noreferrer">
+              <img src={require("../assets/instagram512.svg")} alt="Instagram" className="App-social-logo" />
+            </a>
+          </div>
+          <div>
+            <a href="https://www.linkedin.com/in/ppprues/" target="_blank" rel="noopener noreferrer">
+              <img src={require("../assets/linkedin512.svg")} alt="LinkedIn" className="App-social-logo" />
+            </a>
+          </div>
         </div>
-        <div>
-          <a href="https://github.com/ppprues/" target="_blank" rel="noopener noreferrer">
-            <img src={require("../assets/github512.svg")} alt="GitHub" className="App-social-logo" />
-          </a>
-        </div>
-        <div>
-          <a href="https://instagram.com/ppprues/" target="_blank" rel="noopener noreferrer">
-            <img src={require("../assets/instagram512.svg")} alt="Instagram" className="App-social-logo" />
-          </a>
-        </div>
-        <div>
-          <a href="https://www.linkedin.com/in/ppprues/" target="_blank" rel="noopener noreferrer">
-            <img src={require("../assets/linkedin512.svg")} alt="LinkedIn" className="App-social-logo" />
-          </a>
-        </div>
-      </div>
+      </CSSTransitionGroup>
     </div>
   );
 }
